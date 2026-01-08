f23dd51a6dfca37ebc669f8a6a17005d5910008e
hra2zithub <hr.a2zithub@gmail.com>
2026-01-08 14:33:59 +0530

    Update dbconnection.js

--- routes/connection.js
+++ routes/connection.js
@@ -3,10 +3,10 @@ var util=require('util')
 
 
 var conn=mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:"hotel_management_system"
+    host:"bmmcycfikksqaa4hdrwu-mysql.services.clever-cloud.com",
+    user:"uudbnps9cnq5gyls",
+    password:"pgdedfGJPUfOc2I2MjtY",
+    database:"bmmcycfikksqaa4hdrwu"
 })
 var exe=util.promisify(conn.query).bind(conn)
 
